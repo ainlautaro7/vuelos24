d0c76f0506c2dc5ff514f80d32594cb3cb724ef8
Ainlautaro <ainlautaro7@gmail.com>
2021-11-23 22:28:46 -0300

    Update ajax.js
    se agregaron campos ocultos utiles para las consultas

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -14,8 +14,8 @@ function obtenerPasajeros() {
 
     var peticion = ObtenerXHR();
     var claseBoleto = document.getElementById("claseBoleto").value;
-    var nroVuelo = document.getElementById("nroVuelo").value;
-     
+    var nroVuelo = document.getElementById("nroVueloActual").value;
+
     peticion.open("GET", "http://localhost/vuelos24/public/php/api.php?opcion=1&claseBoleto=" + claseBoleto + "&nroVuelo=" + nroVuelo, true);
     peticion.onreadystatechange = cargarResultados;
     peticion.send(null);
@@ -32,13 +32,13 @@ function obtenerPasajeros() {
 
                 for (var i = 0; i < obj.length; i++) {
                     pasajeros += "<div class='form-check my-3 mx-2'>" +
-                        "<input type='checkbox' onChange='obtenerVuelos()' name='nroPasajero" + (i + 1) + "' id='nroPasajero" + (i + 1) + "' value = nroPasajero'" + i +"'>" +
-                        "<input type='hidden' name='nombrePasajero" + (i + 1) + "' id='nombrePasajero" + (i + 1) +  "' value = '" + obj[i].nombrePasajero +"'>" +
-                        "<input type='hidden' name='documentoPasajero" + (i + 1) + "' id='documentoPasajero" + (i + 1) +  "' value = '" + obj[i].documentoPasajero +"'>" +
-                        "<input type='hidden' name='apellidoPasajero" + (i + 1) + "' id='apellidoPasajero" + (i + 1) + "' value = '" + obj[i].apellidoPasajero +"'>" +
-                        "<input type='hidden' name='codCliente" + (i + 1) + "' id='codCliente" + (i + 1) + "' value = '" + obj[i].codCliente +"'>" +
-                        "<input type='hidden' name='tipoBoleto" + (i + 1) + "' id='tipoBoleto" + (i + 1) + "' value = '" + obj[i].tipoBoleto +"'>" +
-                        "<input type='hidden' name='estadoBoleto" + (i + 1) + "' id='estadoBoleto" + (i + 1) + "' value = '" + obj[i].estadoBoleto +"'>" +
+                        "<input type='checkbox' onChange='obtenerVuelos()' name='documentoPasajero" + (i + 1) + "' id='documentoPasajero" + (i + 1) + "' value = "+obj[i].documentoPasajero+">" +
+                        "<input type='hidden' name='nombrePasajero" + (i + 1) + "' id='nombrePasajero" + (i + 1) + "' value = '" + obj[i].nombrePasajero + "'>" +
+                        // "<input type='hidden' name='documentoPasajero" + (i + 1) + "' id='documentoPasajero" + (i + 1) + "' value = '" + obj[i].documentoPasajero + "'>" +
+                        "<input type='hidden' name='apellidoPasajero" + (i + 1) + "' id='apellidoPasajero" + (i + 1) + "' value = '" + obj[i].apellidoPasajero + "'>" +
+                        "<input type='hidden' name='codCliente" + (i + 1) + "' id='codCliente" + (i + 1) + "' value = '" + obj[i].codCliente + "'>" +
+                        "<input type='hidden' name='tipoBoleto" + (i + 1) + "' id='tipoBoleto" + (i + 1) + "' value = '" + obj[i].tipoBoleto + "'>" +
+                        "<input type='hidden' name='estadoBoleto" + (i + 1) + "' id='estadoBoleto" + (i + 1) + "' value = '" + obj[i].estadoBoleto + "'>" +
                         "<label class='ps-2 form-check-label' for='nombrePasajero" + (i + 1) + "'>" + obj[i].nombrePasajero + " " + obj[i].apellidoPasajero + "</label>" +
                         "<label class='ps-2 form-check-label' for='documentoPasajero" + (i + 1) + "'>" + obj[i].documentoPasajero + "</label>" +
                         "</div>";
@@ -50,7 +50,7 @@ function obtenerPasajeros() {
             }
         }
     }
-    
+
 }
 
 function obtenerVuelos() {
@@ -58,8 +58,10 @@ function obtenerVuelos() {
     var claseBoleto = document.getElementById("claseBoleto").value;
     var origenVuelo = document.getElementById("origenVuelo").value;
     var destinoVuelo = document.getElementById("destinoVuelo").value;
-    var nroVuelo = document.getElementById("nroVuelo").value;
+    var nroVuelo = document.getElementById("nroVueloActual").value;
     var nroPasajeros = document.querySelectorAll('input[type=checkbox]:checked').length;
+
+    document.getElementById('cantPasajeros').value = nroPasajeros;
 
     if (nroPasajeros > 0) {
         peticion.open("GET", "http://localhost/vuelos24/public/php/api.php?opcion=2&nroPasajeros=" + nroPasajeros + "&claseBoleto=" + claseBoleto + "&nroVuelo=" + nroVuelo + "&origenVuelo=" + origenVuelo + "&destinoVuelo=" + destinoVuelo + "&nroVuelo=" + nroVuelo, true);
@@ -78,18 +80,17 @@ function obtenerVuelos() {
 
                     for (var i = 0; i < obj.length; i++) {
 
-                        vuelos += "<div class='card' style='width: 18rem;'>" +
+                        vuelos += "<label class='form-check-label mx-2' for='nroVueloSeleccionado" + (i + 1) + "'>" +
+                            "<div class='card' style='width: 18rem;'>" +
                             "<div class='card-body'>" +
-
-                            "<input type='hidden' id='vuelo" + (i + 1) + "' name='vuelo" + (i + 1) + "' value='" + obj[i].nroVuelo + "'>" +
-
+                            "<input class='form-check-input' type='radio' name='nroVueloSeleccionado' id='nroVueloSeleccionado" + (i + 1) + "' value='" + obj[i].nroVuelo + "'></input>" +
                             "<h5 class='card-title text-center'>Vuelo Nro " + obj[i].nroVuelo + "</h5>" +
-                            "<hr>"+
+                            "<hr>" +
                             "<p>Fecha del Vuelo: " + obj[i].fechaVuelo + "</p>" +
                             "<p>Hora del Vuelo: " + obj[i].horaVuelo + "</p>" +
-                            "<a type='submit' class='btn btn-primary'>Reasignar pasajeros a este vuelo</a>" +
                             "</div>" +
-                            "</div>";
+                            "</div>" +
+                            "</label>";
                     }
                     filtro3.innerHTML = vuelos;
                 }
